9ea68b738b6029ed10d45c93d584da46f980c37f
lan.nguyen2k <47696901+lannguyen0910@users.noreply.github.com>
2020-12-17 22:53:56 +0700

    add Profile Settings Fragments

---
+++ app/src/main/java/com/KLK/photogallery/profile/EditProfileFragment.java
@@ -0,0 +1,24 @@
+package com.KLK.photogallery.profile;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.KLK.photogallery.R;
+
+public class EditProfileFragment extends Fragment {
+    // For debugging
+    private static final String TAG = "EditProfileFragment";
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_edit_profile, container, false);
+        return view;
+    }
+}
