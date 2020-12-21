5142da6291200c99395d7f6f9c5ca061be6b7cb5
lan.nguyen2k <47696901+lannguyen0910@users.noreply.github.com>
2020-12-22 06:25:51 +0700

    add Homepage UI

--- app/src/main/java/com/KLK/photogallery/home/MainActivity.java
+++ app/src/main/java/com/KLK/photogallery/home/MainActivity.java
@@ -1,6 +1,7 @@
 package com.KLK.photogallery.home;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Context;
 import android.os.Bundle;
@@ -22,11 +23,29 @@ public class MainActivity extends AppCompatActivity {
     private static final int ACTIVITY_NUM = 0;
     private Context context = MainActivity.this;
 
+    private String[] imageUrls = new String[]{
+            "https://i.pinimg.com/originals/ff/f6/fa/fff6fa7b1d480f2a298630cb789a4fe1.jpg",
+            "https://previews.123rf.com/images/nguyenthanhtrong/nguyenthanhtrong2001/nguyenthanhtrong200100067/141533444-happy-new-year-2021-with-firework-background-firework-display-colorful-for-holidays-.jpg",
+            "https://previews.123rf.com/images/nguyenthanhtrong/nguyenthanhtrong2001/nguyenthanhtrong200100089/136892288-happy-new-year-2021-with-firework-background-firework-display-colorful-for-holidays-.jpg",
+            "https://previews.123rf.com/images/nguyenthanhtrong/nguyenthanhtrong2001/nguyenthanhtrong200100072/141533446-happy-new-year-2021-with-firework-background-firework-display-colorful-for-holidays-.jpg",
+            "https://previews.123rf.com/images/nguyenthanhtrong/nguyenthanhtrong2001/nguyenthanhtrong200100110/136897247-happy-new-year-2021-with-firework-background-firework-display-colorful-for-holidays-.jpg",
+            "https://static.toiimg.com/thumb/msid-73028722,imgsize-1432792,width-800,height-600,resizemode-75/73028722.jpg",
+            "https://img.freepik.com/free-vector/happy-new-year-2021-numbers-green-fir-branches-holiday-ornaments-white-background-greeting-card-promotion-poster-template_145666-1191.jpg?size=626&ext=jpg",
+            "https://thumbs.dreamstime.com/b/happy-new-year-golden-d-numbers-ribbons-confetti-white-background-happy-new-year-167245897.jpg",
+            "https://happynewyear2021.net/wp-content/uploads/2019/09/happy-new-year-2021-new-820x554.png",
+            "https://image.winudf.com/v2/image1/Y29tLmhhcHB5bmV3eWVhcjIwMjAuaGFwcHluZXd5ZWFyLm5ld3llYXIyMDIwLkdpZjIwMjBfc2NyZWVuXzBfMTU5ODcxOTk3OF8wNzc/screen-0.jpg?h=355&fakeurl=1&type=.jpg"
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(TAG, "Start onCreate()!");
+
+        ViewPager viewPager = findViewById(R.id.viewpager_container);
+        ViewPagerAdapter adapter = new ViewPagerAdapter(this, imageUrls);
+        viewPager.setAdapter(adapter);
+
         configBottomNavigationView();
     }
 
