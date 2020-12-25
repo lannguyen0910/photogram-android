3cb8d8654740de8d810386c566f4bb375f734a6c
lan.nguyen2k <47696901+lannguyen0910@users.noreply.github.com>
2020-12-26 01:29:55 +0700

    add SplashScreen

---
+++ Photogram/app/src/main/java/com/KLK/photogallery/splash_screen/SplashScreenActivity.java
@@ -0,0 +1,52 @@
+package com.KLK.photogallery.splash_screen;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.KLK.photogallery.R;
+import com.KLK.photogallery.login.LoginActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private static final int SPLASHSCREEN = 5000;
+    //Variables
+    Animation topAnim, botAnim;
+    ImageView image;
+    TextView logo, slogan;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.layout_splash_screen);
+        image = findViewById(R.id.splashImage);
+        logo = findViewById(R.id.textView);
+        slogan = findViewById(R.id.textView2);
+
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        botAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+        image.setAnimation(topAnim);
+        logo.setAnimation(botAnim);
+        slogan.setAnimation(botAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASHSCREEN);
+
+    }
+}
